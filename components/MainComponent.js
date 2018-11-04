1e6f31dc205d61c5c55e7196765750ab95040210
Micah Mutrux <micah.mutrux@gmail.com>
2018-11-04 10:06:07 -0500

    NAVIGATION: add drawer navigation
    Created a new Home Component which serves as a new home screen.
    
    Added the drawer navigation with two menu items: Home & Menu. We
    also retain the previous stack navigation for menu items.

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -1,31 +1,68 @@
-import React, { Component } from 'react';
-import { View, Platform } from 'react-native';
+import React, {Component} from 'react';
+import {View, Platform} from 'react-native';
+import Home from './HomeComponent';
 import Menu from './MenuComponent';
-import { DISHES } from '../shared/dishes';
 import DishDetail from './DishDetailComponent';
-import { createStackNavigator } from 'react-navigation';
+import {createStackNavigator, createDrawerNavigator} from 'react-navigation';
+import {Icon} from 'react-native-elements';
 
 class Main extends Component {
 
 	render() {
 
 		const MenuNavigator = createStackNavigator(
-			{
-				Menu: { screen: Menu },
-				DishDetail: { screen: DishDetail }
+			{ // Manifest of possible screens
+				Menu: {screen: Menu},
+				DishDetail: {screen: DishDetail}
 			},
-			{
+			{ // Default config for all screens
 				initialRouteName: 'Menu',
 				navigationOptions: {
-					headerStyle: { backgroundColor: '#512DA8' },
+					headerStyle: {backgroundColor: '#512DA8'},
 					headerTintColor: '#fff',
 				}
 			}
-		)
+		);
+
+		const HomeNavigator = createStackNavigator(
+			{ // Manifest of possible screens
+				Home: {screen: Home}
+			},
+			{ // Default config for all screens
+				initialRouteName: 'Home',
+				navigationOptions: ({ navigation }) => ({
+					headerStyle: {backgroundColor: '#512DA8'},
+					headerTintColor: '#fff',
+				})
+			}
+		);
+
+
+		const MainNavigator = createDrawerNavigator(
+			{ // Manifest of possible screens
+				Home: {
+					screen: HomeNavigator,
+					navigationOptions: {
+						title: 'Home',
+						drawerLabel: 'Home'
+					}
+				},
+				Menu: {
+					screen: MenuNavigator,
+					navigationOptions: {
+						title: 'Menu',
+						drawerLabel: 'Menu'
+					}
+				}
+			},
+			{ // Default config for all screens
+				drawerBackgroundColor: '#D1C4E9' // light purple
+			}
+		);
 
 		return (
-			<View style={{ flex: 1, paddingTop: Platform.OS === 'ios' ? 0 : Expo.Constants.statusBarHeight }}>
-				<MenuNavigator />
+			<View style={{flex: 1, paddingTop: Platform.OS === 'ios' ? 0 : Expo.Constants.statusBarHeight}}>
+				<MainNavigator/>
 			</View>
 		);
 	}
